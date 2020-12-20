5007bdc3e9311f0bb817c5e3912ffde1a2da932b
mouhamedazizbensmida <azizbensmida0@gmail.com>
2020-12-20 11:53:53 +0100

    first commit

---
+++ src/Components/CardTemplate/CardTemplate.jsx
@@ -0,0 +1,21 @@
+/* @flow */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card } from 'antd'
+
+
+const CardTamplate = ({ name, date, vote, image, id }) => (
+  <Link to={`/movie/${id}`}>
+    <Card style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
+      <div className='custom-image'>
+        <img alt={name} width='100%' src={`https://image.tmdb.org/t/p/w500${image}`} />
+      </div>
+      <div className='custom-card'>
+        <h3>{name}</h3>
+        <p>{`Date: ${date} || Votes: ${vote}`}</p>
+      </div>
+    </Card>
+  </Link>
+)
+
+export default CardTamplate
---
+++ src/Components/inscription.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchForm from '../SearchForm/SearchForm'
+import ListTable from '../Table/Table'
+
+const inscription = () => (
+  <div>
+    <SearchForm />
+    <ListTable />
+  </div>
+)
+
+export default inscription
